29b5d52ff0986e94e8cdf7d98419562cb53c9622
GogoMogo1989 <kovacsfarkasdavid@gmail.com>
2022-10-08 21:49:31 +0200

    20221008

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import './App.css';
-import ProductSlider from './ProductSlider';
+import './App.css'
+import Swiper from './components/Swiper';
+
+import "swiper/css/bundle";
+import "./index.css";
 
 function App() {
   return (
-   <ProductSlider />
+   <Swiper />
   );
 }
 
---
+++ src/components/Swiper.jsx
@@ -0,0 +1,44 @@
+import React, { useRef, useState } from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+import "./swiper.css";
+
+// import required modules
+import { Pagination, Navigation } from "swiper";
+
+export default function App() {
+  return (
+    <>
+      <Swiper
+        slidesPerView={1}
+        spaceBetween={30}
+        loop={true}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Pagination, Navigation]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-1.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-2.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-3.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-4.jpg" />
+        </SwiperSlide>
+      </Swiper>
+    </>
+  );
+}
